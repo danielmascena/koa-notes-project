5dd853085a4630f410c9db371915b2e31baf31be
Daniel Mascena <816224+danielmascena@users.noreply.github.com>
2024-07-25 18:53:17 +0100

    remove old code commented

--- app.js
+++ app.js
@@ -42,45 +42,6 @@ router.get("/logout", (ctx) => {
   ctx.session.authenticated = false;
   ctx.redirect("/login");
 });
-/*
-app.use(async (ctx, next) => {
-  if (ctx.path !== "/") {
-    return await next();
-  }
-  if (ctx.session.authenticated) {
-    return (ctx.body = "Hello back");
-  }
-  ctx.status = 401;
-});
-
-app.use(async (ctx, next) => {
-  if (ctx.path !== "/login") {
-    return await next();
-  }
-  if (ctx.request.method === "GET") {
-    return (ctx.body = form);
-  }
-  if (ctx.request.method !== "POST") {
-    return;
-  }
-  const body = await parse(ctx);
-  console.log({ body });
-
-  if (body.username !== "username" || body.password !== "password") {
-    return (ctx.status = 500);
-  }
-  ctx.session.authenticated = true;
-  ctx.redirect("/");
-});
-
-app.use(async (ctx, next) => {
-  if (ctx.path !== "/logout") {
-    return await next();
-  }
-  ctx.session.authenticated = false;
-  ctx.redirect("/login");
-});
-*/
 app.use(router.routes()).use(router.allowedMethods());
 
 app.listen(3009, (err) => {
