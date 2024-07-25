55e5466ed9e00d28d64697cc70be9799906bc08a
Daniel Mascena <816224+danielmascena@users.noreply.github.com>
2024-07-25 18:52:06 +0100

    convert to routes

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const Koa = require("koa");
+const Router = require("@koa/router");
 const parse = require("co-body");
 const session = require("koa-session");
 
 const app = new Koa();
+const router = new Router();
 const form = `
   <form action="/login" method="POST">
     <input name="username" type="text" value="username">
@@ -15,6 +17,32 @@ app.keys = ["secret1", "secret2", "secret3"];
 
 app.use(session(app));
 
+router.get("/", (ctx, next) => {
+  if (ctx.session.authenticated) {
+    return (ctx.body = "hello back");
+  }
+  ctx.status = 401;
+});
+
+router.get("/login", (ctx, next) => {
+  ctx.body = form;
+});
+
+router.post("/login", (ctx) => {
+  return parse(ctx).then(({ username, password }) => {
+    if (username !== "username" || password !== "password") {
+      return (ctx.status = 500);
+    }
+    ctx.session.authenticated = true;
+    ctx.redirect("/");
+  });
+});
+
+router.get("/logout", (ctx) => {
+  ctx.session.authenticated = false;
+  ctx.redirect("/login");
+});
+/*
 app.use(async (ctx, next) => {
   if (ctx.path !== "/") {
     return await next();
@@ -52,7 +80,13 @@ app.use(async (ctx, next) => {
   ctx.session.authenticated = false;
   ctx.redirect("/login");
 });
+*/
+app.use(router.routes()).use(router.allowedMethods());
 
-app.listen(3009);
-console.log("Server up and running on port 3009");
-console.log("🚀");
+app.listen(3009, (err) => {
+  if (err) {
+    console.error(err.stack);
+  }
+  console.log("Server up and running on port 3009");
+  console.log("🚀");
+});
